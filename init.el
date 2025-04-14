1ae1a67758ed9064f8d08c014281c7a8c9e3fede
Giovanni Meo <giovannimeo@gmail.com>
2025-04-14 12:35:38 +0200

    Start emacs 30 customization

---
+++ init.el
@@ -0,0 +1,10 @@
+(message "Emacs 30 init.el")
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name
+        "straight/repos/straight.el/bootstrap.el"
+        (or (bound-and-true-p straight-base-dir)
+            user-emacs-directory)))
+      (bootstrap-version 7))
+  (load bootstrap-file nil 'nomessage))
+(message "End of Emacs 30 init.el")
