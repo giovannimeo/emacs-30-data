d63582c37748e119aaf3c1eadcb4fd3105f153b2
Giovanni Meo <giovannimeo@gmail.com>
2025-04-14 13:17:11 +0200

    Use straigt package manager

---
+++ early-init.el
@@ -0,0 +1,4 @@
+(message "Emacs 30 early-init.el")
+(message "Disable default package manager")
+(setq package-enable-at-startup nil)
+(message "End of Emacs 30 early-init.el")
--- init.el
+++ init.el
@@ -1,4 +1,5 @@
 (message "Emacs 30 init.el")
+(message "Initialize straight package manager")
 (defvar bootstrap-version)
 (let ((bootstrap-file
        (expand-file-name
@@ -7,4 +8,9 @@
             user-emacs-directory)))
       (bootstrap-version 7))
   (load bootstrap-file nil 'nomessage))
+
+(message "List of packages to install")
+(straight-use-package 'use-package)
+(straight-use-package 'yaml-mode)
+(message "Customizations for user")
 (message "End of Emacs 30 init.el")
